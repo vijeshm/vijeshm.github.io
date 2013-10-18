dcccd0a6416aac5c840854c7c81da7826fa6d512
Vijesh  M <mv.vijesh@gmail.com>
2013-10-19 02:35:46 +0530

    added resume button

--- js/vijeshJS.js
+++ js/vijeshJS.js
@@ -11,51 +11,9 @@ function activate(id) {
 	$('#tab_' + id).addClass('selected')
 }
 
-function shuffle(array) {
-  var currentIndex = array.length
-    , temporaryValue
-    , randomIndex
-    ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
-shuffle(imageNames);
-fadeInOutTime = 2000;
-imageDuration = 3000;
-currentImageIndex = 0;
-
-function fadeOut() {
-	$('#backgroundChild').css('background', 'rgb(0, 0, 0)');
-	setTimeout(changeBackground, fadeInOutTime);
-	setTimeout(fadeIn, fadeInOutTime);
-}
-
-function fadeIn() {
-	$('#backgroundChild').css('background', 'rgba(0, 0, 0, 0)');
-	setTimeout(fadeOut, fadeInOutTime + imageDuration)
-}
-
-function changeBackground() {
-	$('#backgroundContainer').css('background-image', 'url("../images/' + imageNames[currentImageIndex] + '.jpg")');
-	currentImageIndex += 1;
-	if(currentImageIndex == imageNames.length) {
-		currentImageIndex = 0;
-		shuffle(imageNames);
-	}
-}
-
-changeBackground();
-fadeIn();
+$('body').css({
+ 	'background-image' : 'url("../images/' + imageNames[Math.floor(Math.random() * imageNames.length)] + '.jpg")',
+	'background-size' : 'cover',
+	'background-position' : '50% 50%',
+	'background-repeat': 'none'
+});
