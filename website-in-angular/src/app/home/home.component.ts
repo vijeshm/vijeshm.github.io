cac11af11c8dc49dfb600ebde26337ed1d288ee0
Vijesh Mohan <vmohan@akamai.com>
2020-05-17 23:44:49 +0530

    added twitter feed and youtube link

--- website-in-angular/src/app/home/home.component.ts
+++ website-in-angular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ISocialMediaElement } from '../types';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public imagePath = '../../assets/images/mylogo.png';
   public name = 'Vijesh Mohan';
+  public tags: string[] = ['Engineer', 'Photographer', 'Researcher', 'A restless soul'];
+  public socialMediaLinks: ISocialMediaElement[] = [{
+    socialMediaLink: 'http://github.com/vijeshm',
+    iconPath: '../../assets/images/social-media/github.png'
+  }, {
+    socialMediaLink: 'http://flickr.com/vijeshm',
+    iconPath: '../../assets/images/social-media/flickr.png'
+  }, {
+    socialMediaLink: 'http://linkedin.com/pub/vijesh-m/48/683/780',
+    iconPath: '../../assets/images/social-media/linkedin.png'
+  }, {
+    socialMediaLink: 'http://facebook.com/mv.vijesh',
+    iconPath: '../../assets/images/social-media/facebook.png'
+  }, {
+    socialMediaLink: 'http://twitter.com/edbidangi',
+    iconPath: '../../assets/images/social-media/twitter.png'
+  }, {
+    socialMediaLink: 'https://plus.google.com/114081513617733356807/posts',
+    iconPath: '../../assets/images/social-media/googleplus.png'
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
+    (window as any).twttr.widgets.load();
   }
 
 }
---
+++ website-in-angular/src/app/types.ts
@@ -0,0 +1,4 @@
+export interface ISocialMediaElement {
+    socialMediaLink: string;
+    iconPath: string;
+}
