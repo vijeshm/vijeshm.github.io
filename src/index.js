11adc7f23e193daed8409c40ea594528568afcd6
Vijesh Mohan <mv.vijesh@gmail.com>
2022-05-25 11:33:57 +0530

    Trying out hash routes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import TutorialApp from "./TutorialApp";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Route, Routes } from "react-router-dom";
+import { About, Events } from "./pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let isAuthorized = false;
 
 root.render(
-  <Router>
-    <TutorialApp login="vijeshm" />
-  </Router>
+  <HashRouter>
+    <Routes>
+      <Route exact path="/" element={<About />} />
+      <Route exact path="/events" element={<Events />} />
+    </Routes>
+  </HashRouter>
+
+  // <Router>
+  //   <TutorialApp login="vijeshm" />
+  // </Router>
+
   // <App />
   // <ul>
   //   <li>Monday</li>
